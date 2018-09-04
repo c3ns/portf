498bcbe3c1fb84c9ab56bed58e3f11059fd40e72
c3ns <ernestas.radzvilas@gmail.com>
2018-09-04 16:55:05 +0300

    social

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,30 @@ import React from 'react';
 const Footer = (props) => {
     return(
         <footer>
-            <p>© {(new Date()).getFullYear()} (Beta 0.8.16)</p>
+            <div className="container">
+                <div className="wrapper">
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.linkedin.com/in/ernestas-radzvilas"
+                    >
+                        <i className="fab fa-linkedin"/>
+                    </a>
+                    <p>View my Linkedin profile</p>
+                </div>
+                <div className="wrapper">
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/c3ns/"
+                    >
+                        <i className="fab fa-github-square"/>
+                    </a>
+                    <p>View my GitHub</p>
+                </div>
+                <div className="clearfix"/>
+            </div>
+            <p>© {(new Date()).getFullYear()}</p>
         </footer>
     )
 };
